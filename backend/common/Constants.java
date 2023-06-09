440e558946347eba477fc23d9235b2955de8c353
Qiu Qingyang <65663690+qqy-1120@users.noreply.github.com>
2023-06-09 15:28:23 +0800

    Add files via upload

---
+++ backend/common/Constants.java
@@ -0,0 +1,9 @@
+package com.example.demo.common;
+
+public interface Constants {
+    String CODE_200 = "200";// 请求成功
+    String CODE_400 = "400";// 400	Bad Request 参数错误
+    String CODE_401 = "401";// 401	Unauthorized 权限不足
+    String CODE_500 = "500";// 500	Internal Server Error 服务器错误
+    String CODE_600 = "600";// 其他业务异常
+}
