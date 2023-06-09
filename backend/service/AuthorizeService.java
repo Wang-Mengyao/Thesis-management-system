628b367766d37bd1ff0dcbe4b4c03f35dae53620
Qiu Qingyang <65663690+qqy-1120@users.noreply.github.com>
2023-06-09 15:39:20 +0800

    Add files via upload

---
+++ backend/service/AuthorizeService.java
@@ -0,0 +1,10 @@
+package com.example.demo.service;
+
+import com.example.demo.common.Result;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public interface AuthorizeService extends UserDetailsService {
+
+//    Result login(String username, String password);
+    Result register(String username, String password, String email);
+}
